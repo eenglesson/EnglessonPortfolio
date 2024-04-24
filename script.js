0bead3cfb9b658fe23abc7bfe43a2354a786db62
eenglesson <eliasenglesson00@gmail.com>
2024-04-24 13:01:34 +0200

    1

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+  hamburgerToggle();
+  handleScroll();
+});
+
+function hamburgerToggle() {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const menuLinks = document.querySelector('.menu-links');
+  const hamburgerIcon = hamburgerMenu.querySelector('.hamburger-icon');
+
+  hamburgerMenu.addEventListener('click', () => {
+    menuLinks.classList.toggle('open');
+    hamburgerIcon.classList.toggle('active');
+  });
+}
+
+function handleScroll() {
+  const specificScrollPosition = 100;
+
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const hamburgerIcon = hamburgerMenu.querySelector('.hamburger-icon');
+  const menuLinks = document.querySelector('.menu-links');
+
+  window.addEventListener('scroll', function () {
+    if (
+      menuLinks.classList.contains('open') &&
+      window.scrollY > specificScrollPosition
+    ) {
+      menuLinks.classList.remove('open');
+      hamburgerIcon.classList.remove('active');
+    }
+  });
+}
